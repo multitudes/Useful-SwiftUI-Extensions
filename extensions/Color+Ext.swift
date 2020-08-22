b7e4c96e5f08bac78761207924e42d9611d4d5c8
laurent <wrmultitudes@gmail.com>
2020-08-22 15:53:47 +0200

    Update Color+Ext.swift

--- extensions/Color+Ext.swift
+++ extensions/Color+Ext.swift
@@ -30,20 +30,20 @@ extension Color {
 // This extension allows to create a Color in swiftUI from the hex value
 // ex Color(hex: "FA1859") or Color(hex: "F19") and with opacity as hex number at the end
 extension Color {
-    init(hex: String) {
+    init(hex: String, opacity a: Int = 255) {
         let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
         var int: UInt64 = 0
         Scanner(string: hex).scanHexInt64(&int)
-        let r, g, b, a: UInt64
+        let r, g, b: UInt64
         switch hex.count {
         case 3: // RGB (12-bit)
-            (r, g, b, a) = ((int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17, 255)
+            (r, g, b) = ((int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
         case 6: // RGB (24-bit)
-            (r, g, b, a) = (int >> 16, int >> 8 & 0xFF, int & 0xFF, 255)
+            (r, g, b) = (int >> 16, int >> 8 & 0xFF, int & 0xFF)
         case 8: // RGBA (32-bit)
-            (r, g, b, a) = (int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF, int >> 24)
+            (r, g, b) = (int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
         default:
-            (r, g, b, a) = (1, 1, 1, 1)
+            (r, g, b) = (255, 255, 255)
         }
 
         self.init(
