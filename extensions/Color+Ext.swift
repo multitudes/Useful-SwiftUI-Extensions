0de240b161778403f4c6191b3a959690195ca7a7
laurent <wrmultitudes@gmail.com>
2020-08-23 16:37:42 +0200

    some small edit

--- extensions/Color+Ext.swift
+++ extensions/Color+Ext.swift
@@ -9,28 +9,37 @@ import Foundation
 import SwiftUI
 
 // This extension will create predefined static colors that can be used in my app like: Color.lightRed or Color.darkRed. I use a custom init to create colors from decimal values between 0-255
+// I made these colors accessible with a white text as foreground
+// see https://contrast-ratio.com/#%23e5145f-on-%23FFFdFb
 extension Color {
-    public static var outlineRed: Color {
-        return Color(decimalRed: 34, green: 0, blue: 3)
+    public static var outlineLightRed: Color {
+        return Color(hex: "FF5898")
+    }
+
+    public static var gradientStartRed: Color {
+        return Color(hex: "E61164")
+    }
+    
+    public static var gradientEndRed: Color {
+        return Color(hex: "E41859")
+    }
+    public static var buttonLightRed: Color {
+        return Color(hex: "CA4275")
     }
     
     public static var darkRed: Color {
-        return Color(decimalRed: 221, green: 31, blue: 59)
+        return Color(hex: "FF5898")
     }
     
-    public static var lightRed: Color {
-            return Color(decimalRed: 239, green: 54, blue: 128)
-        }
-    
-    public init(decimalRed red: Double, green: Double, blue: Double) {
-        self.init(red: red/255, green: green/255, blue: blue/255)
+    public static var lightTitle: Color {
+        return Color(hex: "FFFdFb")
     }
 }
 
 // This extension allows to create a Color in swiftUI from the hex value
 // ex Color(hex: "FA1859") or Color(hex: "F19") and with opacity as hex number at the end
 extension Color {
-    init(hex: String, opacity a: Int = 255) {
+    init(hex: String, opacity a: Double = 1.0) {
         let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
         var int: UInt64 = 0
         Scanner(string: hex).scanHexInt64(&int)
@@ -49,7 +58,7 @@ extension Color {
             red: Double(r) / 255,
             green: Double(g) / 255,
             blue:  Double(b) / 255,
-            opacity: Double(a) / 255
+            opacity: a
         )
     }
 }
